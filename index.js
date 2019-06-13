806437b5e547b3ab5cec4039471570136c91ac38
Kelly de Vries <kdv@me.com>
2019-06-13 09:30:49 -0700

    add initial practice query and express server

---
+++ index.js
@@ -0,0 +1,33 @@
+// index.js
+
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+let schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+let root = {
+	hello: function() {
+		return 'Hello world! 🎉';
+	},
+};
+
+const app = express();
+app.get('/', function(req, res) {
+	res.send('Express is working! 😘');
+});
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema: schema,
+		rootValue: root,
+		graphiql: true,
+	}),
+);
+app.listen(4000, function() {
+	console.log('Listening on port 4000');
+});
